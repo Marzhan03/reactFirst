04b27179d220beb65a9cf38acb2a1ffe74a83420
NurayDuck <rnmnrmtruexl@gmail.com>
2024-01-12 19:32:22 +0600

    before transition

--- node/my-app/src/store/store.js
+++ node/my-app/src/store/store.js
@@ -1,9 +1,11 @@
-// store.js
-
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension'; // Import the necessary DevTools extension functions
 import { thunk } from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
