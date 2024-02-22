926b1aafe7e29f595942ec8b30dea106cb2163a9
Lifanna <solano.lifan2@bk.ru>
2024-02-22 22:05:54 +0600

    temp commit

--- node/my-app/src/components/main/Main.js
+++ node/my-app/src/components/main/Main.js
@@ -4,22 +4,25 @@ import MainNavbar from './MainNavbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import HeaderListThree from './headerListThree';
+import Footer from '../Footer'
 
 
 const Main = () => {
     return (
-        <><div className={`container-fluid ${styles.background}`}>
-            <Row className="justify-content-center mx-4">
-                <Col md={10}>
-                    <Row>
-                        <Col lg={12}>
-                            <div><MainNavbar /></div>
-                            <div><HeaderListThree/></div>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </div>
+        <>
+            <div className={`container-fluid background`}>
+                <Row className="justify-content-center">
+                    <Col md={10}>
+                        <div><MainNavbar /></div>
+                        <div><HeaderListThree /></div>
+                    </Col>
+                </Row>
+                <Row className='mt-5'>
+                    <Col style={{ background: "#393DA1" }} md={12}>
+                        <Footer />
+                    </Col>
+                </Row>
+            </div>
         </>
 
     )
--- node/my-app/src/components/main/MainNavbar.js
+++ node/my-app/src/components/main/MainNavbar.js
@@ -3,30 +3,38 @@ import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
 import logoImage from '../../assets/images/logo.png';
 import styles from '../../assets/css/main.module.css';
-import style from '../../assets/css/mainnavbar.module.css';
+import mainNavbarStyles from '../../assets/css/mainnavbar.module.css';
 import styler from '../../assets/css/loginnavbar.module.css';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom';
 
 const MainNavbar = () => {
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        navigate('/login');
+    };
+
     return (
-        <Row>
-            <Col className='d-flex justify-content-center align-items-center' lg={6} md={12} sm={12}>
-                    <div className='d-flex justify-content-center align-items-center me-5'>
-                        <Image src={logoImage} style={{ width: '50px', height: '50px' }} alt="React Logo" className={styles.logo_png} />
-                        <div className={`${styler.logo_title}`}>TANU PRO</div>
-                    </div>
-                    <Link className={style.navLeftLink} to="/">Главная</Link>
-                    <Link className={style.navLeftLink} to="/">FAQ</Link>
-                    <Link className={style.navLeftLink} to="/">Новости</Link>
+        // <Row>
+        <Row className={`mx-0 ${mainNavbarStyles.bgNavbar}`}>
+            <Col className={`d-flex justify-content-center align-items-center py-3`} lg={6} md={12} sm={12}>
+                <div className='d-flex justify-content-center align-items-center me-5'>
+                    <Image src={logoImage} style={{ width: '50px', height: '50px' }} alt="React Logo" className={styles.logo_png} />
+                    <div className={`${styler.logo_title}`}>TANU PRO</div>
+                </div>
+                <Link className={mainNavbarStyles.navLeftLink} to="/">Главная</Link>
+                <Link className={mainNavbarStyles.navLeftLink} to="/">FAQ</Link>
+                <Link className={mainNavbarStyles.navLeftLink} to="/">Новости</Link>
             </Col>
-            <Col className='d-flex justify-content-center align-items-center' lg={6} md={12} sm={12}>
+            <Col className='d-flex justify-content-center align-items-center py-3' lg={6} md={12} sm={12}>
                 <div className={`d-flex justify-content-end ${styles.navRight}`}>
-                    <Link className={style.navLink} to="/">Семье</Link>
-                    <Link className={style.navLink} to="/">Школам</Link>
-                    <Link className={style.navLink} to="/">Бизнес</Link>
-                    <Button className={`${styles.loginButton} rounded-pill px-5`}>Войти</Button>
+                    <Link className={mainNavbarStyles.navLink} to="/">Семье</Link>
+                    <Link className={mainNavbarStyles.navLink} to="/">Школам</Link>
+                    <Link className={mainNavbarStyles.navLink} to="/">Бизнес</Link>
+                    <Button onClick={handleLogin} className={`${styles.loginButton} rounded-pill px-5`}>Войти</Button>
                 </div>
             </Col>
         </Row>
